99df42dc621114c4ec16b66747e4b2fd56532dad
AkashDeep1234455 <118648099+AkashDeep1234455@users.noreply.github.com>
2024-05-15 13:43:10 +0530

    Time: 296 ms (68.01%), Space: 90.3 MB (26.25%) - LeetHub

--- 2623-memoize/2623-memoize.js
+++ 2623-memoize/2623-memoize.js
@@ -3,48 +3,13 @@
  * @return {Function}
  */
 function memoize(fn) {
-
-    let hashMap = {
-        sum:{},
-        fib:{},
-        factorial:{},
-    }
-    
+    let cache = new Map();
     return function(...args) {
-        if(args.length==2){
-            let a = args[0];
-            let b = args[1];
-            let key = `${a},${b}`;
-            if(key in hashMap.sum) return hashMap.sum[key];
-            else{
-
-                let ans = fn(...args);
-                hashMap.sum[key] = ans;
-                return ans;
-            }
-        }else{
-        
-            let a = args[0];
-            let key = `${a}`;
-            if(key in hashMap.fib) return hashMap.fib[key];
-            else{
-            
-                let ans  = fn(...args);
-                hashMap.fib[key] = ans;
-                return ans;
-            }
-        
-        
-            let b= args[0];
-            let key2 = `${b}`;
-            if(key2 in hashMap.factorial) return hashMap.factorial[key2];
-            else{
-                
-                let ans = fn(...args);
-                hashMap.factorial[key2] = ans;
-                return ans;
-            }       
-    }
+        let key = JSON.stringify(args);
+        if(key in cache) return cache[key];
+        const val = fn(...args);
+        cache[key] = val;
+        return val;
     }
 }
 
