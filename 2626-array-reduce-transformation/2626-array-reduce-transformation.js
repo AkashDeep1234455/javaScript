d87eb6784cfe88e97dfc5cb0fba1c1ed7e8a9be3
AkashDeep1234455 <118648099+AkashDeep1234455@users.noreply.github.com>
2024-05-15 12:38:43 +0530

    Time: 57 ms (36.21%), Space: 49.3 MB (43.41%) - LeetHub

--- 2626-array-reduce-transformation/2626-array-reduce-transformation.js
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -6,11 +6,8 @@
  */
 var reduce = function(nums, fn, init) {
     if(nums.length==0) return init;
-    // for(let i=0;i<nums.length;i++){
-    //     init= fn(init,nums[i]);
-    // }
-    nums.forEach((element,index)=>{
-        init = fn(init,element);
-    })
+    for(let i=0;i<nums.length;i++){
+        init= fn(init,nums[i]);
+    }
     return init;
 };
